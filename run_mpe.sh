8bd758918f057e9e95dedceda78aee98481886a5
Kupchinsky <user@killer666.ru>
2015-03-29 09:06:13 +0700

    Updated mpe script

--- run_mpe.sh
+++ run_mpe.sh
@@ -8,9 +8,18 @@ then
 	exit
 fi
 
+check_exit_code()
+{
+	if [ $1 -gt 0 ]; then
+		echo Failed! Exit code: $1
+		exit
+	fi
+}
+
 echo "!!Running $filename [$*] on $pc processes..."
 export LD_LIBRARY_PATH=/home/gcc/objdir/tempinstall/home/gcc/gcc-4.9.2/lib64
 mpiexec -n "$pc" "$filename" $*
+check_exit_code $?
 echo "!!End running $filename"
 
 if [ -e "$filename.clog2" ]; then
