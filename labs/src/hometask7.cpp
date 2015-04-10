fafba4fc52a66411ce98a5357f0ad215bd23872d
Kupchinsky <user@killer666.ru>
2015-04-10 20:00:14 +0700

    updated

--- labs/src/hometask7.cpp
+++ labs/src/hometask7.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+int** splitpointers(int *arr, int n, int split_size)
+{
+	int size = n / split_size, **result = new int*[size];
+
+	for (int i = 0; i < size; i++)
+		result[i] = arr + (split_size * i);
+
+	return result;
+}
+
 int main(int argc, char** argv)
 {
 	int rank, size, n;
@@ -13,51 +23,54 @@ int main(int argc, char** argv)
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
 
 	if (rank == 0)
-		//n = atoi(argv[1]);
-		scanf("%d", &n);
+		n = atoi(argv[1]);
+		//scanf("%d", &n);
 
 	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	int *A = new int[n], *B = new int[size * n];
+	int *A = new int[n], *singleB, **B;
 
 	if (rank == 0)
 	{
-		for (int i = 0; i < size * n; i++)
-			B[i] = i;
+		// Заполняем матрицу
+		singleB = new int[size * n];
+		B = splitpointers(singleB, size * n, n);
+
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < n; j++)
+				B[i][j] = i + j;
+		}
 	}
 
-	MPI_Scatter(B, n, MPI_INT, A, n, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Scatter(B[0], n, MPI_INT, A, n, MPI_INT, 0, MPI_COMM_WORLD);
 
 	// Выводим строку
-	string buf;
+	stringstream ss;
 
 	for (int i = 0; i < n; i++)
-	{
-		stringstream ss;
-		ss << A[i];
-		buf += ss.str() + " ";
-	}
+		ss << A[i] << " ";
 
-	printf("Rank: %d, Arr: %s\n", rank, buf.c_str());
-
+	printf("Rank: %d, Arr: %s\n", rank, ss.str().c_str());
+/*
 	if (rank == 0)
 	{
 		// Дополнительно выводим всю матрицу
-		buf = "";
+		ss.clear();
 
-		for (int i = 0; i < size * n; i++)
+		for (int i = 0; i < size; i++)
 		{
-			if (i % n == 0)
-				buf += "\n";
+			ss << "(" << n << ") ";
 
-			stringstream ss;
-			ss << B[i];
-			buf += ss.str() + " ";
+			for (int j = 0; j < n; j++)
+				ss << B[i][j] << " ";
+
+			ss << endl;
 		}
 
-		printf("Source Arr:%s\n", buf.c_str());
+		printf("Source Arr:\n%s\n", ss.str().c_str());
 	}
-
+*/
 	MPI_Finalize();
 	return 0;
 }
